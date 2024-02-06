03b10c1284b2d213e586198093152511c113c136
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-06 14:49:47 -0600

    ascii drawing

--- src/components/ConsoleComponent.jsx
+++ src/components/ConsoleComponent.jsx
@@ -13,7 +13,15 @@ export default function ConsoleComponent() {
   const dispatch = useDispatch();
   const [terminalData, setTerminalData] = useState([
     "Bienvenido a mi website",
-    "Yo soy cheko version 1.0.0",
+    String.raw`
+      ________         __            ______                _      
+      / ____/ /_  ___  / /______     / ________ ___________(_____ _
+    / /   / __ \/ _ \/ //_/ __ \   / / __/ __  / ___/ ___/ / __  /
+    / /___/ / / /  __/ ,< / /_/ /  / /_/ / /_/ / /  / /__/ / /_/ / 
+    \____/_/ /_/\___/_/|_|\____/   \____/\__,_/_/   \___/_/\__,_/  
+                                                                  
+  `,
+    "version 1.0.0",
     "teclea la palabra init y presiona enter",
   ]);
 
