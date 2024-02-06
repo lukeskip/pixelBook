d79ba9ae8326b19b0f621550479977ee5d527157
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-06 17:24:35 -0600

    small design fixes

--- src/commands/index.js
+++ src/commands/index.js
@@ -11,7 +11,7 @@ class Bash {
     this[cmd] ? this[cmd](args) : this.error();
   }
 
-  print(args) {
+  question(args) {
     return this.dispatch(setDialog(args));
   }
 
@@ -20,12 +20,32 @@ class Bash {
     return this.dispatch(setConsole("close"));
   }
 
+  help(args) {
+    this.dispatch(
+      setTerminal([
+        "type a command and press enter, you can run any of the following commands:",
+        "command1 command2 command3",
+      ])
+    );
+    return this.dispatch(setConsole("open"));
+  }
+
   stop(args) {
     this.dispatch(setTerminal(["stop"]));
     return this.dispatch(setConsole("open"));
   }
-  error() {
-    this.dispatch(setTerminal(["Ese comando no existe intenta nueva mente"]));
+
+  error(message) {
+    if (!message) {
+      this.dispatch(
+        setTerminal([
+          "bash: command not found",
+          "To see a list of supported commands, run: help",
+        ])
+      );
+    } else {
+      this.dispatch(setTerminal([message]));
+    }
   }
 }
 
