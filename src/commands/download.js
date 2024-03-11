ff3590309adf756cf0d06174604badbe5522b28d
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-11 11:29:20 -0600

    download cv complete flow with languages

--- src/commands/download.js
+++ src/commands/download.js
@@ -2,7 +2,8 @@ import strings from '../utils/strings'
 // It allows to download the resumé in PDF, it receives a "flag" so it can deliver different versions of the resume
 const HOST = import.meta.env.VITE_URL_HOST;
 export default function download(args) {
-  this._consoleMessage([strings[this.language].wait]);
-  window.open(`${HOST}/download/cv.pdf`, '_blank');
+  this._consoleMessage(strings[this.language].downloading);
+  this._delay(strings[this.language].ready);
+  window.open(`${HOST}/download/cv.pdf?lang=${this.language}`, '_blank');
 
 }
