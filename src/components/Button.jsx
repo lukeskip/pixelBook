961d06036138aa66d80d6447d2168b680b518ea2
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-12 16:24:02 -0600

    test end

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// library.add(fas, fab);
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
 
 export default function Button({label,icon,callback}) {
   return (
     <div className='button' onClick={callback}>
           <div className="icon">
-              {/* <FontAwesomeIcon icon={[icon.collection, icon.name]} /> */}
+              <FontAwesomeIcon icon={[icon.collection, icon.name]} />
           </div>
           <div className="label">
               {label}
