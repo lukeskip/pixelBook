00d47e452a9c0cb61dfdedbceeefbbf12bd848bf
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-15 22:31:43 -0600

    test

--- src/apiRequest/questionRequest.js
+++ src/apiRequest/questionRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios"; 
 const HOST = import.meta.env.VITE_URL_HOST;
 const questionRequest = async (question, lang = "es") => {
+  console.log(HOST);
   return await axios.post(HOST, {
     question,
     lang,
